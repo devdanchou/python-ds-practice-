ab47ed6d07fa91e9f513b2b25b0762ec20125c0f
Daniel Chou <daniel.cw.chou@gmail.com>
2023-07-17 19:40:45 -0400

    worked on question 7

--- 07_multiple_letter_count.py
+++ 07_multiple_letter_count.py
@@ -7,3 +7,13 @@ def multiple_letter_count(phrase):
         >>> multiple_letter_count('Yay')
         {'Y': 1, 'a': 1, 'y': 1}
     """
+    letter_to_count = {}
+
+    for char in phrase:
+        if char in letter_to_count:
+            letter_to_count[char] += 1
+        else:
+            letter_to_count[char] = 1
+    #    letter_to_count[char] = (letter_to_count[char] or 0) + 1
+
+    return letter_to_count
